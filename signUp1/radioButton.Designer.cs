446b75b0888f6397c1f3ae20c2ee77024359cff6
Radhitia Pratama <radhitia43@gmail.com>
2023-07-11 19:34:59 +0700

    test radio button component

---
+++ signUp1/radioButton.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace signUp1
+{
+    partial class radioButton
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioBtnFemale = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioBtnMale = new System.Windows.Forms.RadioButton();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioBtnFemale
+            // 
+            this.radioBtnFemale.AutoSize = true;
+            this.radioBtnFemale.Location = new System.Drawing.Point(213, 83);
+            this.radioBtnFemale.Name = "radioBtnFemale";
+            this.radioBtnFemale.Size = new System.Drawing.Size(59, 17);
+            this.radioBtnFemale.TabIndex = 0;
+            this.radioBtnFemale.TabStop = true;
+            this.radioBtnFemale.Text = "Female";
+            this.radioBtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gender";
+            // 
+            // radioBtnMale
+            // 
+            this.radioBtnMale.AutoSize = true;
+            this.radioBtnMale.Location = new System.Drawing.Point(159, 83);
+            this.radioBtnMale.Name = "radioBtnMale";
+            this.radioBtnMale.Size = new System.Drawing.Size(48, 17);
+            this.radioBtnMale.TabIndex = 2;
+            this.radioBtnMale.TabStop = true;
+            this.radioBtnMale.Text = "Male";
+            this.radioBtnMale.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(159, 115);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Submit Gender";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btmSubmit_Click);
+            // 
+            // radioButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.radioBtnMale);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioBtnFemale);
+            this.Name = "radioButton";
+            this.Text = "radioButton";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioBtnFemale;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioBtnMale;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
---
+++ signUp1/radioButton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace signUp1
+{
+    public partial class radioButton : Form
+    {
+        public radioButton()
+        {
+            InitializeComponent();
+        }
+
+        private void btmSubmit_Click(object sender, EventArgs e)
+        {
+            string gender;
+
+            if(!radioBtnFemale.Checked && !radioBtnMale.Checked)
+            {
+                MessageBox.Show("Please select your gender !");
+                return;
+            }
+
+            if (radioBtnFemale.Checked)
+            {
+                gender = radioBtnFemale.Text;
+                MessageBox.Show(gender);
+            }
+
+            if (radioBtnMale.Checked)
+            {
+                gender = radioBtnMale.Text;
+                MessageBox.Show(gender);
+            }
+        }
+    }
+}
