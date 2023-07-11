b121f59be75869de09183572b16b988581e0f248
Radhitia Pratama <radhitia43@gmail.com>
2023-07-11 19:56:26 +0700

    test dialogbox

---
+++ signUp1/dialogBox.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace signUp1
+{
+    partial class dialogBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(217, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(343, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Visual Studio 2019 Proffesional";
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Location = new System.Drawing.Point(222, 66);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(338, 40);
+            this.btnInstall.TabIndex = 1;
+            this.btnInstall.Text = "Install";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.SystemColors.Control;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblResult.Location = new System.Drawing.Point(178, 135);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 25);
+            this.lblResult.TabIndex = 2;
+            // 
+            // dialogBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnInstall);
+            this.Controls.Add(this.label1);
+            this.Name = "dialogBox";
+            this.Text = "dialogBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInstall;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
---
+++ signUp1/dialogBox.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace signUp1
+{
+    public partial class dialogBox : Form
+    {
+        public dialogBox()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            string info = "" +
+                "1.Beli Visual Studio 2019 Proffesional (Press Yes) \n" +
+                "2.Gunakan versi trial 30 hari (Press No) \n" +
+                "3.Batalkan Installasi (Press Cancel)";
+
+            lblResult.Text = "";
+
+            DialogResult v_dialogResult = MessageBox.Show(info,
+                "Pilih Jenis Installasi",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Information);
+
+            if(v_dialogResult == DialogResult.Yes)
+            {
+                lblResult.Text = "Anda membeli Visual Studio 2019 Proffesional !";
+            }
+
+            if(v_dialogResult == DialogResult.No)
+            {
+                lblResult.Text = "Anda menggunakan Visual Studio Trial";
+            }
+
+            if(v_dialogResult == DialogResult.Cancel)
+            {
+                lblResult.Text = "Installasi dibatalkan";
+            }
+        }
+    }
+}
